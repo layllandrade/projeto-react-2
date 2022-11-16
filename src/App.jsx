4967708c6b416548ecdfab427ce3fdc3ad30a4fc
Laylla Andrade <layllandradef@live.com>
2022-11-15 23:32:47 -0300

    Adicionando logica do componente Repositorios dentro do componente search

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import { Header } from './components/Header'
 import { Bio } from './components/Bio'
 import { Search } from './components/Search'
-import { Repositorios } from './components/Repositorios'
 import { Footer } from './components/Footer'
 
 
@@ -12,7 +11,6 @@ function App() {
       <Header content="Meu github search" />
       <Bio />
       <Search />
-      <Repositorios />
       <Footer content="Feito com ♡ por Laylla. E todos os direitos são reservados." />
     </div>
   )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,45 @@
+import {useState, useEffect } from 'react'
+import Axios from 'axios'
+
 export function Search() {
     function pegaInput(event) {
         console.log(event.target.value)
     }
+
     return (
-        <div className="Search">
-            <input placeholder="Digite um repo" onChange={pegaInput} />
+        <>
+            <div className="Search">
+                <input placeholder="Digite um repo" onChange={pegaInput} />
+            </div>
+            <Repositorios />
+        </>
+    )
+}
+
+export function Repositorios() {   
+    const [reposFromApi, setReposFromApi] = useState([])
+    const baseURL ='https://api.github.com/users/layllandrade/repos'
+
+    useEffect (() => {
+        async function getData(){
+            const response = await Axios.get(baseURL)
+            setReposFromApi(response.data)
+        }
+
+        getData()
+    }, [])
+
+    return (
+        <div>
+            {reposFromApi.map(item => {
+                return (
+                    <div key={item.id}> 
+                        <h3>{item.name}</h3>
+                        <p>{item.description}</p>
+                        <a href={item.html_url}>Conferir</a>
+                    </div>
+                )
+            })}
         </div>
     )
 }
